49faf80d6fa80127b9fe7885cd64ea1d77f8b5b7
akanishii <42301583+akanishii@users.noreply.github.com>
2019-01-05 12:57:01 -0500

    Add files via upload

---
+++ Analysis.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 30 14:32:11 2018
+
+@author: malinda
+"""
+
+
+def Data_Analysis(file):
+    import pandas as pd
+    data=pd.read_csv(file)
+    return data.describe(include='all')
