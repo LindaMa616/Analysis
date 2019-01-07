0d2b3156a2b5c771789a56550db6e5df2d5246e2
akanishii <42301583+akanishii@users.noreply.github.com>
2019-01-06 19:02:28 -0500

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -7,5 +7,6 @@ Created on Sun Dec 30 14:32:11 2018
 
 def Data_Analysis(file):
     import pandas as pd
-    data=pd.read_csv(file)
-    return data.describe(include='all')
+    data=pd.read_csv(file).describe(include='all').transpose()
+    data.to_csv('Data_Analysis.csv')
+    return data
