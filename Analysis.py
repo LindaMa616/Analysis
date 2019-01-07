d7399ebb3122e0fa60e66dea142c96b51ffcdac1
akanishii <42301583+akanishii@users.noreply.github.com>
2019-01-06 19:23:44 -0500

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -9,6 +9,10 @@ Created on Sun Dec 30 14:32:11 2018
 
 def Data_Analysis(file):
     import pandas as pd
+    #describe function could return the descriptive statistics that summarize the distribution 
+    #if not adding include='all' inside describe(), it will only return the distribution of continous variables, if adding, it will also return the summary of discrete variables
+    #transpose is to permute the dimensions
     data=pd.read_csv(file).describe(include='all').transpose()
+    #.to_csv is used to export the data analysis results to working directory, the file name is 'Data_Analysis'
     data.to_csv('Data_Analysis.csv')
     return data
